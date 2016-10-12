ade266450e35a3c2eff69917138e23a56b9ad3a9
haya14busa <hayabusa1419@gmail.com>
2016-10-13 05:19:37 +0900

    add test data

---
+++ diff/testdata/gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+diff -u sample.{orig,new}.txt > sample.diff
+diff -u nonewline.{orig,new}.txt > nonewline.diff
+diff -u nonewline2.{orig,new}.txt > nonewline2.diff
+diff -u nonewline3.{orig,new}.txt > nonewline3.diff
