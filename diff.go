488cdf8d52f7c75000cef65fa8f7048c7fea0646
haya14busa <hayabusa1419@gmail.com>
2016-10-17 01:05:23 +0900

    add diff test

--- diff.go
+++ diff.go
@@ -1,5 +1,9 @@
 package watchdogs
 
+import (
+	"os/exec"
+)
+
 var _ DiffService = &DiffString{}
 
 type DiffString struct {
@@ -16,5 +20,24 @@ func (d *DiffString) Diff() ([]byte, error) {
 }
 
 func (d *DiffString) Strip() int {
+	return d.strip
+}
+
+var _ DiffService = &DiffCmd{}
+
+type DiffCmd struct {
+	cmd   *exec.Cmd
+	strip int
+}
+
+func NewDiffCmd(cmd *exec.Cmd, strip int) DiffService {
+	return &DiffCmd{cmd: cmd, strip: strip}
+}
+
+func (d *DiffCmd) Diff() ([]byte, error) {
+	return d.cmd.Output()
+}
+
+func (d *DiffCmd) Strip() int {
 	return d.strip
 }
---
+++ diff_test.go
@@ -0,0 +1,52 @@
+package watchdogs
+
+import "io/ioutil"
+
+import "os/exec"
+import "strings"
+import "testing"
+
+func TestDiffString(t *testing.T) {
+	difftext := `diff --git a/golint.old.go b/golint.new.go
+index 34cacb9..a727dd3 100644
+--- a/golint.old.go
++++ b/golint.new.go
+@@ -2,6 +2,12 @@ package test
+ 
+ var V int
+ 
++var NewError1 int
++
+ // invalid func comment
+ func F() {
+ }
++
++// invalid func comment2
++func F2() {
++}
+`
+	d := NewDiffString(difftext, 1)
+	b, err := d.Diff()
+	if err != nil {
+		t.Error(err)
+	}
+	got := string(b)
+	if got != difftext {
+		t.Errorf("got:\n%v\nwant:\n%v", got, difftext)
+	}
+}
+
+func TestDiffCmd(t *testing.T) {
+	wantb, err := ioutil.ReadFile("./diff/testdata/golint.diff")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := strings.SplitN(string(wantb), "\n", 5)[4] // strip extended header
+	cmd := exec.Command("git", "diff", "--no-index", "./diff/testdata/golint.old.go", "./diff/testdata/golint.new.go")
+	d := NewDiffCmd(cmd, 1)
+	b, _ := d.Diff()
+	got := strings.SplitN(string(b), "\n", 5)[4]
+	if got != want {
+		t.Errorf("got:\n%v\nwant:\n%v", got, want)
+	}
+}
