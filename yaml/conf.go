7e12faf781bbcc321565c129106eb5de63ee1647
haya14busa <hayabusa1419@gmail.com>
2016-12-11 22:20:30 +0900

    yaml: introduce config struct and Parse func

---
+++ yaml/conf.go
@@ -0,0 +1,37 @@
+// Package yaml provides utility of reviewdog config yaml file.
+package yaml
+
+import yaml "gopkg.in/yaml.v2"
+
+// Config represents reviewdog config.
+type Config struct {
+	Runner map[string]*Runner
+}
+
+// Runner represents config for a runner.
+type Runner struct {
+	// Runner comamnd. (e.g. `golint ./...`)
+	Cmd string
+
+	// tool name in review comment. (e.g. `golint`)
+	Name string
+	// errorformat name. (e.g. `checkstyle`)
+	Format string
+	// errorformat. (e.g. `%f:%l:%c:%m`)
+	Errorformat string
+}
+
+// Parse parses reviewdog config in yaml format.
+func Parse(yml []byte) (*Config, error) {
+	out := &Config{}
+	if err := yaml.Unmarshal(yml, out); err != nil {
+		return nil, err
+	}
+	// Insert `Name` field if it's empty.
+	for name, runner := range out.Runner {
+		if runner.Name == "" {
+			runner.Name = name
+		}
+	}
+	return out, nil
+}
