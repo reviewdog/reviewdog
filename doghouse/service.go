c474e3a8fd8ce2d055cb0138a0e0d275ee794ab9
haya14busa <haya14busa@gmail.com>
2018-05-13 12:05:00 +0000

    doghouse: omitempty

--- doghouse/service.go
+++ doghouse/service.go
@@ -5,27 +5,27 @@ type CheckRequest struct {
 	// Installation ID of reviewdog GitHub APP.
 	// https://github.com/settings/installations/<INSTALLATION_ID>
 	// You can find from here: https://github.com/settings/installations/
-	InstallationID int `json:"installation_id"`
+	InstallationID int `json:"installation_id,omitempty"`
 
-	SHA         string `json:"sha"`
-	PullRequest int    `json:"pull_request"`
-	Owner       string `json:"owner"`
-	Repo        string `json:"repo"`
+	SHA         string `json:"sha,omitempty"`
+	PullRequest int    `json:"pull_request,omitempty"`
+	Owner       string `json:"owner,omitempty"`
+	Repo        string `json:"repo,omitempty"`
 
 	// Name of the annotation tool.
 	// Optional.
-	Name        string        `json:"name"`
-	Annotations []*Annotation `json:"annotations"`
+	Name        string        `json:"name,omitempty"`
+	Annotations []*Annotation `json:"annotations,omitempty"`
 }
 
 // GitHub Check Response
 type CheckResponse struct {
-	ReportURL string `json:"report_url"`
+	ReportURL string `json:"report_url,omitempty"`
 }
 
 type Annotation struct {
-	Path       string `json:"path"`        // relative file path
-	Line       int    `json:"line"`        // line number
-	Message    string `json:"message"`     // message
-	RawMessage string `json:"raw_message"` // original error message
+	Path       string `json:"path,omitempty"`        // relative file path
+	Line       int    `json:"line,omitempty"`        // line number
+	Message    string `json:"message,omitempty"`     // message
+	RawMessage string `json:"raw_message,omitempty"` // original error message
 }
