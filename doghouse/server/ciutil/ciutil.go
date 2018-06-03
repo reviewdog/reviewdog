b4ee09667146dccfb1d6476723d20a7adfc9a040
haya14busa <haya14busa@gmail.com>
2018-06-03 12:46:13 +0000

    doghouse: add default travis CI IP addresses

--- doghouse/server/ciutil/ciutil.go
+++ doghouse/server/ciutil/ciutil.go
@@ -9,8 +9,51 @@ import (
 )
 
 var (
+	// Set Travis CI addrs by default though UpdateTravisCIIPAddrs will update it
+	// with latest one.
+	// $ dig +short nat.travisci.net | sort
+	// # Updated on 2018-06-03.
 	muTravisIPAddrs sync.RWMutex
-	travisIPAddrs   = map[string]bool{}
+	travisIPAddrs   = map[string]bool{
+		"104.154.113.151": true,
+		"104.154.120.187": true,
+		"104.197.236.150": true,
+		"146.148.51.141":  true,
+		"146.148.58.237":  true,
+		"147.75.192.163":  true,
+		"207.254.16.35":   true,
+		"207.254.16.36":   true,
+		"207.254.16.37":   true,
+		"207.254.16.38":   true,
+		"207.254.16.39":   true,
+		"34.233.56.198":   true,
+		"34.234.4.53":     true,
+		"35.184.226.236":  true,
+		"35.184.48.144":   true,
+		"35.184.96.71":    true,
+		"35.188.184.134":  true,
+		"35.188.1.99":     true,
+		"35.188.73.34":    true,
+		"35.192.136.167":  true,
+		"35.192.187.174":  true,
+		"35.192.19.50":    true,
+		"35.192.217.12":   true,
+		"35.192.85.2":     true,
+		"35.193.203.142":  true,
+		"35.193.211.2":    true,
+		"35.193.7.13":     true,
+		"35.202.145.110":  true,
+		"35.202.68.136":   true,
+		"35.202.78.106":   true,
+		"35.224.112.202":  true,
+		"35.226.126.204":  true,
+		"52.3.55.28":      true,
+		"52.45.185.117":   true,
+		"52.45.220.64":    true,
+		"52.54.31.11":     true,
+		"52.54.40.118":    true,
+		"54.208.31.17":    true,
+	}
 
 	// https://www.appveyor.com/docs/build-environment/#ip-addresses
 	appveyorIPAddrs = map[string]bool{
