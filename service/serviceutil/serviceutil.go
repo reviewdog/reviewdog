3411825536385b90b2e44f8bc71e903a51a7b8bc
haya14busa <haya14busa@gmail.com>
2019-03-24 09:49:02 +0000

    Fix golint warning

--- service/serviceutil/serviceutil.go
+++ service/serviceutil/serviceutil.go
@@ -30,6 +30,7 @@ func (p PostedComments) IsPosted(c *reviewdog.Comment, lineNum int) bool {
 	return false
 }
 
+// AddPostedComment adds a posted comment.
 func (p PostedComments) AddPostedComment(path string, lineNum int, body string) {
 	if _, ok := p[path]; !ok {
 		p[path] = make(map[int][]string)
@@ -40,8 +41,10 @@ func (p PostedComments) AddPostedComment(path string, lineNum int, body string) 
 	p[path][lineNum] = append(p[path][lineNum], body)
 }
 
+// BodyPrefix is prefix text of comment body.
 const BodyPrefix = `<sub>reported by [reviewdog](https://github.com/reviewdog/reviewdog) :dog:</sub>`
 
+// CommentBody creates comment body text.
 func CommentBody(c *reviewdog.Comment) string {
 	tool := ""
 	if c.ToolName != "" {
@@ -50,6 +53,7 @@ func CommentBody(c *reviewdog.Comment) string {
 	return tool + BodyPrefix + "\n" + c.Body
 }
 
+// GitRelWorkdir returns git relative workdir of current directory.
 func GitRelWorkdir() (string, error) {
 	b, err := exec.Command("git", "rev-parse", "--show-prefix").Output()
 	if err != nil {
