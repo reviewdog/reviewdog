89e804b4a5a2b106372c458cf0c89894586e9a40
haya14busa <haya14busa@gmail.com>
2019-09-12 13:14:48 +0000

    Stupid code for golint

--- reviewdog.go
+++ reviewdog.go
@@ -10,6 +10,8 @@ import (
 	"github.com/reviewdog/reviewdog/diff"
 )
 
+var X = 0
+
 // Reviewdog represents review dog application which parses result of compiler
 // or linter, get diff and filter the results by diff, and report filtered
 // results.
