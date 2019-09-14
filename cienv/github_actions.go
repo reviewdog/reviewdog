b6d18a9302ea3d9d26beafd43aff31db52482b47
haya14busa <haya14busa@gmail.com>
2019-09-14 16:45:17 +0000

    Support re-run event

--- cienv/github_actions.go
+++ cienv/github_actions.go
@@ -8,19 +8,25 @@ import (
 
 // https://help.github.com/en/articles/virtual-environments-for-github-actions#default-environment-variables
 type GitHubEvent struct {
-	Number      int `json:"number"`
-	PullRequest struct {
-		Head struct {
-			Sha string `json:"sha"`
-			Ref string `json:"ref"`
-		} `json:"head"`
-	} `json:"pull_request"`
-	Repository struct {
+	PullRequest GitHubPullRequest `json:"pull_request"`
+	Repository  struct {
 		Owner struct {
 			Login string `json:"login"`
 		} `json:"owner"`
 		Name string `json:"name"`
 	} `json:"repository"`
+	CheckSuite struct {
+		After        string              `json:"after"`
+		PullRequests []GitHubPullRequest `json:"pull_requests"`
+	} `json:"check_suite"`
+}
+
+type GitHubPullRequest struct {
+	Number int `json:"number"`
+	Head   struct {
+		Sha string `json:"sha"`
+		Ref string `json:"ref"`
+	} `json:"head"`
 }
 
 func getBuildInfoFromGitHubAction() (*BuildInfo, bool, error) {
@@ -43,9 +49,16 @@ func getBuildInfoFromGitHubActionEventPath(eventPath string) (*BuildInfo, bool, 
 	info := &BuildInfo{
 		Owner:       event.Repository.Owner.Login,
 		Repo:        event.Repository.Name,
-		PullRequest: event.Number,
+		PullRequest: event.PullRequest.Number,
 		Branch:      event.PullRequest.Head.Ref,
 		SHA:         event.PullRequest.Head.Sha,
+	}
+	// For re-run check_suite event.
+	if info.PullRequest == 0 && len(event.CheckSuite.PullRequests) > 0 {
+		pr := event.CheckSuite.PullRequests[0]
+		info.PullRequest = pr.Number
+		info.Branch = pr.Head.Ref
+		info.SHA = pr.Head.Sha
 	}
 	return info, info.PullRequest != 0, nil
 }
--- cienv/github_actions_test.go
+++ cienv/github_actions_test.go
@@ -6,12 +6,23 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGetBuildInfoFromGitHubActionEventPath(t *testing.T) {
+func TestGetBuildInfoFromGitHubActionEventPath_prevent(t *testing.T) {
 	got, _, err := getBuildInfoFromGitHubActionEventPath("_testdata/github_event_pull_request.json")
 	if err != nil {
 		t.Fatal(err)
 	}
 	want := &BuildInfo{Owner: "reviewdog", Repo: "reviewdog", SHA: "cb23119096646023c05e14ea708b7f20cee906d5", PullRequest: 285, Branch: "go1.13"}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("result has diff:\n%s", diff)
+	}
+}
+
+func TestGetBuildInfoFromGitHubActionEventPath_rerunevent(t *testing.T) {
+	got, _, err := getBuildInfoFromGitHubActionEventPath("_testdata/github_event_rerun.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &BuildInfo{Owner: "reviewdog", Repo: "reviewdog", SHA: "ba8f36cd3eb401e9de9ee5718e11d390fdbe4afa", PullRequest: 286, Branch: "github-actions-env"}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf("result has diff:\n%s", diff)
 	}
