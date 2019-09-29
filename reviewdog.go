084e7b13c6eb55c22d55854ff74d10081ef8e1bb
haya14busa <haya14busa@gmail.com>
2019-09-29 15:10:55 +0000

    debug: more debug

--- reviewdog.go
+++ reviewdog.go
@@ -10,7 +10,7 @@ import (
 	"github.com/reviewdog/reviewdog/diff"
 )
 
-var X = 14
+var X = 15
 
 // Reviewdog represents review dog application which parses result of compiler
 // or linter, get diff and filter the results by diff, and report filtered
