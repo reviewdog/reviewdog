0a813bb7affeeed0885254a5e30bde4e31816dfc
Ilya Sevostyanov <d3th@zeen.ru>
2019-10-01 20:42:22 +0300

    Redundant parentheses.

--- diff/parse.go
+++ diff/parse.go
@@ -298,7 +298,7 @@ endhunk:
 }
 
 func (p *hunkParser) done(lold, lnew int, hr *hunkrange) bool {
-	end := (lold >= hr.lold+hr.sold && lnew >= hr.lnew+hr.snew)
+	end := lold >= hr.lold+hr.sold && lnew >= hr.lnew+hr.snew
 	if b, err := p.r.Peek(1); err != nil || (string(b) != tokenNoNewlineAtEOF && end) {
 		return true
 	}
