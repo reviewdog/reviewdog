fd8b3c8aa26cc309baf1728a00aa0d545a5d7a39
liuchenchen <liuchenchen.01@bytedance.com>
2020-01-13 15:43:45 +0800

    test
    Change-Id: I78b15847ae32a379b72695dc890fe922cc3c27b1

--- comment.go
+++ comment.go
@@ -1,3 +1,4 @@
+
 package reviewdog
 
 import "context"
@@ -7,6 +8,7 @@ var _ BulkCommentService = &multiCommentService{}
 type multiCommentService struct {
 	services []CommentService
 }
+
 
 func (m *multiCommentService) Post(ctx context.Context, c *Comment) error {
 	for _, cs := range m.services {
