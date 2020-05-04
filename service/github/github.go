59d38c32c814b8eb1176396eacdc06d3764431c6
haya14busa <haya14busa@gmail.com>
2020-05-04 12:42:57 +0000

    remove special IsPosted check for LnumDiff == 0

--- service/github/github.go
+++ service/github/github.go
@@ -83,7 +83,7 @@ func (g *GitHubPullRequest) postAsReviewComment(ctx context.Context) error {
 	comments := make([]*github.DraftReviewComment, 0, len(g.postComments))
 	remaining := make([]*reviewdog.Comment, 0)
 	for _, c := range g.postComments {
-		if c.LnumDiff == 0 || g.postedcs.IsPosted(c, c.LnumDiff) {
+		if g.postedcs.IsPosted(c, c.LnumDiff) {
 			continue
 		}
 		// Only posts maxCommentsPerRequest comments per 1 request to avoid spammy
