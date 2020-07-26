7b3bcaa7b048ead14bd4428912e949219e46e72c
haya14busa <haya14busa@gmail.com>
2020-07-26 08:03:20 +0000

    diff: support unified diff extension used by gofmt -d

---
+++ diff/testdata/gofmt.go
@@ -0,0 +1,20 @@
+package testdata
+
+func    fmt     () {
+	// test
+	// test line
+	// test line
+	// test line
+	// test line
+	// test line
+	// test line
+	// test line
+
+println(
+		"hello, gofmt test"    )
+//comment
+}
+
+
+type s struct { A int }
+func (s s) String() { return "s" }
