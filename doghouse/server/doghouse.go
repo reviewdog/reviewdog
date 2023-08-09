3973e11a45387aa3ad8c0e4a4517802374a9b691
ICHINOSE Shogo <shogo82148@gmail.com>
2023-08-10 01:24:30 +0900

    min and max are now built-in function

--- doghouse/server/doghouse.go
+++ doghouse/server/doghouse.go
@@ -378,10 +378,3 @@ func annotationToDiagnostic(a *doghouse.Annotation) *rdf.Diagnostic {
 		OriginalOutput: a.RawMessage,
 	}
 }
-
-func min(x, y int) int {
-	if x > y {
-		return y
-	}
-	return x
-}
