75e079f3441795e7a9e6d6a87d34f04bbfc01755
haya14busa <haya14busa@gmail.com>
2024-02-12 22:13:26 +0900

    Update proto/rdf/update.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- proto/rdf/update.sh
+++ proto/rdf/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t reviewdog-protoc .
-docker run --rm --volume `pwd`:`pwd` --workdir `pwd` reviewdog-protoc
+docker run --rm --volume $(pwd):$(pwd) --workdir $(pwd) reviewdog-protoc
