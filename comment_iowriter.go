620eaaaeeb7b88f4cbb812cb33f866d28c91cdc9
haya14busa <haya14busa@gmail.com>
2024-07-01 02:14:55 +0900

    Support -reporter=sarif

--- comment_iowriter.go
+++ comment_iowriter.go
@@ -2,9 +2,11 @@ package reviewdog
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 
+	"github.com/haya14busa/go-sarif/sarif"
 	"github.com/reviewdog/reviewdog/proto/rdf"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -141,4 +143,136 @@ func (cw *RDJSONCommentWriter) Flush(_ context.Context) error {
 		return err
 	}
 	return nil
+}
+
+var _ CommentService = &SARIFCommentWriter{}
+
+// SARIFCommentWriter
+type SARIFCommentWriter struct {
+	w        io.Writer
+	comments []*Comment
+	toolName string
+}
+
+func NewSARIFCommentWriter(w io.Writer, toolName string) *SARIFCommentWriter {
+	return &SARIFCommentWriter{w: w, toolName: toolName}
+}
+
+func (cw *SARIFCommentWriter) Post(_ context.Context, c *Comment) error {
+	cw.comments = append(cw.comments, c)
+	return nil
+}
+
+func (cw *SARIFCommentWriter) Flush(_ context.Context) error {
+	run := sarif.Run{
+		Tool: sarif.Tool{
+			Driver: sarif.ToolComponent{
+				Name: cw.toolName,
+			},
+		},
+	}
+	rules := make(map[string]sarif.ReportingDescriptor)
+	for _, c := range cw.comments {
+		result := sarif.Result{
+			Message: sarif.Message{
+				Text: sarif.String(c.Result.Diagnostic.Message),
+			},
+		}
+		if code := c.Result.Diagnostic.GetCode(); code.GetValue() != "" {
+			result.RuleID = sarif.String(code.GetValue())
+			rules[code.GetValue()] = sarif.ReportingDescriptor{
+				ID:      code.GetValue(),
+				HelpURI: sarif.String(code.GetUrl()),
+			}
+		}
+		level := severity2level(c.Result.Diagnostic.GetSeverity())
+		if level != sarif.None {
+			result.Level = &level
+		}
+		artifactLoc := sarif.ArtifactLocation{
+			URI: sarif.String(c.Result.Diagnostic.GetLocation().GetPath()),
+		}
+		result.Locations = []sarif.Location{{
+			PhysicalLocation: &sarif.PhysicalLocation{
+				ArtifactLocation: &artifactLoc,
+				Region:           range2region(c.Result.Diagnostic.GetLocation().GetRange()),
+			},
+		}}
+		if len(c.Result.Diagnostic.GetSuggestions()) > 0 {
+			result.Fixes = make([]sarif.Fix, 0)
+			for _, suggestion := range c.Result.Diagnostic.GetSuggestions() {
+				result.Fixes = append(result.Fixes, sarif.Fix{
+					ArtifactChanges: []sarif.ArtifactChange{
+						{
+							ArtifactLocation: artifactLoc,
+							Replacements: []sarif.Replacement{{
+								DeletedRegion: *range2region(suggestion.GetRange()),
+								InsertedContent: &sarif.ArtifactContent{
+									Text: sarif.String(suggestion.GetText()),
+								},
+							}},
+						},
+					},
+				})
+			}
+		}
+		if len(c.Result.Diagnostic.GetRelatedLocations()) > 0 {
+			result.RelatedLocations = make([]sarif.Location, 0)
+			for _, relLoc := range c.Result.Diagnostic.GetRelatedLocations() {
+				result.RelatedLocations = append(result.RelatedLocations, sarif.Location{
+					PhysicalLocation: &sarif.PhysicalLocation{
+						ArtifactLocation: &sarif.ArtifactLocation{
+							URI: sarif.String(relLoc.GetLocation().GetPath()),
+						},
+						Region: range2region(relLoc.GetLocation().GetRange()),
+					},
+					Message: &sarif.Message{
+						Text: sarif.String(relLoc.Message),
+					},
+				})
+			}
+		}
+		run.Results = append(run.Results, result)
+	}
+	slf := sarif.NewSarif()
+	run.Tool.Driver.Rules = make([]sarif.ReportingDescriptor, 0, len(rules))
+	for _, r := range rules {
+		run.Tool.Driver.Rules = append(run.Tool.Driver.Rules, r)
+	}
+	slf.Runs = []sarif.Run{run}
+	encoder := json.NewEncoder(cw.w)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(slf)
+}
+
+func range2region(rng *rdf.Range) *sarif.Region {
+	region := &sarif.Region{}
+	start := rng.GetStart()
+	end := rng.GetEnd()
+	if start.GetLine() > 0 {
+		region.StartLine = sarif.Int64(int64(start.GetLine()))
+	}
+	if start.GetColumn() > 0 {
+		// Column is not usually unicodeCodePoints, but let's just keep it
+		// as is...
+		region.StartColumn = sarif.Int64(int64(start.GetColumn()))
+	}
+	if end.GetLine() > 0 {
+		region.EndLine = sarif.Int64(int64(end.GetLine()))
+	}
+	if end.GetColumn() > 0 {
+		region.EndColumn = sarif.Int64(int64(end.GetColumn()))
+	}
+	return region
+}
+
+func severity2level(s rdf.Severity) sarif.Level {
+	switch s {
+	case rdf.Severity_ERROR:
+		return sarif.Error
+	case rdf.Severity_WARNING:
+		return sarif.Warning
+	default:
+		return sarif.None
+	}
 }
