0980970d261e6466a84706cce3c20879e8e7409c
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-08 23:31:55 +0900

    fix import path

--- service/github/check.go
+++ service/github/check.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/google/go-github/v70/github"
+	"github.com/google/go-github/v71/github"
 	"github.com/reviewdog/reviewdog"
 	"github.com/reviewdog/reviewdog/cienv"
 	"github.com/reviewdog/reviewdog/filter"
